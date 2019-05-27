6326e1d8b9c15d26a5c025ac6501ccbd60e3bf82
Shiva Chandra <shiva.chandra@swiggy.in>
2019-05-28 02:32:27 +0530

    Added multithreaded detection

---
+++ faceDetectionDNNMultiThread.py
@@ -0,0 +1,128 @@
+# USAGE
+# python faceDetectionDNNMultiThread.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
+
+from imutils.video import VideoStream
+from imutils.video import FPS
+from multiprocessing import Process
+from multiprocessing import Queue
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+def classify_frame(net, inputQueue, outputQueue):
+	while True:
+		# check to see if there is a frame in our input queue
+		if not inputQueue.empty():
+			# grab the frame from the input queue, resize it, and
+			# construct a blob from it
+			frame = inputQueue.get()
+			frame = cv2.resize(frame, (300, 300))
+			blob = cv2.dnn.blobFromImage(frame, 0.007843,
+				(300, 300), 127.5)
+
+			# set the blob as input to our deep learning object
+			# detector and obtain the detections
+			net.setInput(blob)
+			detections = net.forward()
+
+			# write the detections to the output queue
+			outputQueue.put(detections)
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+                help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+                help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# initialize the input queue (frames), output queue (detections),
+# and the list of actual detections returned by the child process
+inputQueue = Queue(maxsize=1)
+outputQueue = Queue(maxsize=1)
+detections = None
+
+# construct a child process *indepedent* from our main process of
+# execution
+print("[INFO] starting process...")
+p = Process(target=classify_frame, args=(net, inputQueue,
+	outputQueue,))
+p.daemon = True
+p.start()
+
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+
+    # if the input queue *is* empty, give the current frame to
+    # classify
+    if inputQueue.empty():
+        inputQueue.put(frame)
+
+    # if the output queue *is not* empty, grab the detections
+    if not outputQueue.empty():
+        detections = outputQueue.get()
+
+    if detections is not None:
+        # loop over the detections
+        for i in range(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with the
+            # prediction
+            confidence = detections[0, 0, i, 2]
+
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence < args["confidence"]:
+                continue
+
+            # compute the (x, y)-coordinates of the bounding box for the
+            # object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # draw the bounding box of the face along with the associated
+            # probability
+            text = "{:.2f}%".format(confidence * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                          (0, 0, 255), 2)
+            cv2.putText(frame, text, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+    fps.update()
+
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
