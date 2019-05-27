05d880c2a908421473c0e9a77a6c219b8383513d
shiva486 <shiva.chandra11@gmail.com>
2019-05-28 03:17:52 +0530

    Removed temp files

--- faceDetectionDNN.py
+++ faceDetectionDNN.py
@@ -9,7 +9,6 @@ import imutils
 import time
 import cv2
 
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
                 help="path to Caffe 'deploy' prototxt file")
@@ -29,41 +28,26 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 fps = FPS().start()
 
-# loop over the frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
 
-    # grab the frame dimensions and convert it to a blob
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
                                  (300, 300), (104.0, 177.0, 123.0))
 
-    # pass the blob through the network and obtain the detections and
-    # predictions
     net.setInput(blob)
     detections = net.forward()
 
-    # loop over the detections
     for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the
-        # prediction
         confidence = detections[0, 0, i, 2]
 
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
         if confidence < args["confidence"]:
             continue
 
-        # compute the (x, y)-coordinates of the bounding box for the
-        # object
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
 
-        # draw the bounding box of the face along with the associated
-        # probability
         text = "{:.2f}%".format(confidence * 100)
         y = startY - 10 if startY - 10 > 10 else startY + 10
         cv2.rectangle(frame, (startX, startY), (endX, endY),
@@ -71,7 +55,6 @@ while True:
         cv2.putText(frame, text, (startX, y),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-    # show the output frame
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
@@ -85,6 +68,5 @@ fps.stop()
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
